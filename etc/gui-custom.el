57ec40f7753065094ebe0fe7c58f38be5a69e14e
YBX <2252176220@qq.com>
2021-02-27 20:37:35 +0900

    添加gui-custim.el

---
+++ etc/gui-custom.el
@@ -0,0 +1,16 @@
+;; 这是使用 GUI 配置自动生成的配置文件
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(benchmark-init use-package smart-mode-line restart-emacs gruvbox-theme))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 210 :width normal)))))
