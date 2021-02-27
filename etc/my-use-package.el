3cb69bfdd5e285078b5bc49a978ea613c446e2f5
YBX <2252176220@qq.com>
2021-02-27 23:09:31 +0900

    添加代码补全

--- etc/my-use-package.el
+++ etc/my-use-package.el
@@ -59,4 +59,34 @@
   :config (setq swiper-action-recenter t
                 swiper-include-line-number-in-search t))
 
+
+
+;;======================================
+;; 补全
+;;=====================================
+(use-package company
+  :config
+  (setq company-dabbrev-code-everywhere t  ;;任何情况都补全
+        company-dabbrev-code-modes t
+        company-dabbrev-code-other-buffers 'all
+        company-dabbrev-downcase nil
+        company-dabbrev-ignore-case t
+        company-dabbrev-other-buffers 'all
+        company-require-match nil
+        company-minimum-prefix-length 2 ;;输入2个字母开始补全
+        company-show-numbers nil
+        company-tooltip-limit 20
+        company-idle-delay 0
+        company-echo-delay 0
+        company-tooltip-offset-display 'scrollbar
+        company-begin-commands '(self-insert-command))
+  (push '(company-semantic :with company-yasnippet) company-backends)
+  :hook ((after-init . global-company-mode)))
+
+
+
+
+
+
+
 (provide 'my-use-package)
--- etc/startup.el
+++ etc/startup.el
@@ -8,8 +8,20 @@
 ;;设置垃圾回收阈值，加速启动速度。
 (setq gc-cons-threshold most-positive-fixnum)
 
-
 ;;关闭备份
 (setq make-backup-files nil auto-save-default nil)
+
+;;关闭锁文件
+(setq create-lockfiles nil)
+
+;;总是加载最新的文件
+(setq load-prefer-newer t)
+
+;;关闭字体缓存
+(setq inhibit-compacting-font-caches nil)
+
+;;记录上一次关闭位置，打开自动跳转
+(save-place-mode 1)
+
 
 (provide 'startup)
