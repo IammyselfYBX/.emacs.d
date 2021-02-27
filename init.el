a4aa668f10889fe12663dd241929a6b250f37837
YBX <2252176220@qq.com>
2021-02-27 18:14:51 +0900

    图形化修改配置安放位置

--- init.el
+++ init.el
@@ -2,20 +2,16 @@
 (add-to-list 'load-path
 	     (expand-file-name (concat user-emacs-directory "etc")))
 
+;;emacs使用图形化工具自动生成的配置写入
+(setq custom-file
+    (expand-file-name "custom.el" (concat user-emacs-directory "etc")))
+(when (file-exists-p custom-file)
+  (load-file custom-file))
+
+
 (require 'startup)
 (require 'init-ui)
 ;;软件源
 (require 'init-elpa)
 (require 'my-use-package)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(restart-emacs use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
