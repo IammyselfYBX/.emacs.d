e5b69f5223a182d7d30748c8b842ddb6e329595d
tony <2252176220@qq.com>
2023-06-16 17:05:03 +0800

    新建gui-custom.el

---
+++ etc/gui-custom.el
@@ -0,0 +1,14 @@
+;; 该配置文件存储由 gui 模式下修改配置生成的代码
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(gruvbox-theme restart-emacs ## use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
