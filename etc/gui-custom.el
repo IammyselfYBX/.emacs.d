82be02d4d467623421df9108785602425160a718
tony <2252176220@qq.com>
2023-06-17 20:25:20 +0800

    esc替代C-g失败

--- etc/gui-custom.el
+++ etc/gui-custom.el
@@ -6,7 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dashboard cnfonts smart-mode-line gruvbox-theme restart-emacs ## use-package)))
+   '(company counsel ivy dashboard cnfonts smart-mode-line gruvbox-theme restart-emacs ## use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
