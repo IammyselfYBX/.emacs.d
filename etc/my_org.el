99490ddf8bca2d37e0e89e4c4ae5e21d35a13285
tony <2252176220@qq.com>
2023-07-12 21:02:39 +0800

    新增日历的内容

--- etc/my_org.el
+++ etc/my_org.el
@@ -241,7 +241,7 @@
 (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
 
 ;; orgmode 调整图片尺寸
-;;(setq org-image-actual-width nil) ;; 使用图像的实际宽度
+(setq org-image-actual-width nil) ;; 使用图像的实际宽度
 
 ;;==========================================================
 ;; Latex
@@ -303,6 +303,15 @@
 ;;==========================================================
 ;;导出
 ;;==========================================================
+;; 生成目录
+;; toc-org
+;; https://github.com/snosov1/toc-org
+;; 使用方法
+;;     ~C-c C-q~ 在地下的 Tags: 写 `toc`
+;;     然后按 ~C-x C-s~ 保存的时候就会出现目录
+(use-package toc-org 
+  :hook (org-mode . toc-org-mode))
+
 ;;----------------------------------------------------------
 ;; 导出成 PDF —— ox-latex
 ;; ox-latex 是Org mode自带的功能，可以将Org文件导出为latex文件和PDF文件。
@@ -412,6 +421,104 @@
 ;;==========================================================
 ;; Aganda设置
 ;;==========================================================
+;; org-todo
+;;  C-c C-t (org-todo) 来设置 TODO 状态，状态的种类由 org-todo-keywords 这个变量定义，默认值为 TODO 和 DONE
+;(setq org-todo-keywords '((sequence "TODO(t)" "HOLD(h!)" "WIP(i!)" "WAIT(w!)" "|" "DONE(d!)" "CANCELLED(c@/!)")
+;					      (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f!)")))
+
+
+;;----------------------------------------------------------
+;; 对 日历 Calendar 设置
+(use-package calendar
+  :ensure nil
+  :hook (calendar-today-visible . calendar-mark-today)
+  :custom
+  ;; 是否显示中国节日，我们使用 `cal-chinese-x' 插件
+  (calendar-chinese-all-holidays-flag nil)
+  ;; 是否显示节日
+  (calendar-mark-holidays-flag t)
+  ;; 是否显示Emacs的日记，我们使用org的日记
+  (calendar-mark-diary-entries-flag nil)
+  ;; 数字方式显示时区，如 +0800，默认是字符方式如 CST
+  (calendar-time-zone-style 'numeric)
+  ;; 日期显示方式：year/month/day
+  (calendar-date-style 'iso)
+  ;; 中文天干地支设置
+  (calendar-chinese-celestial-stem ["甲" "乙" "丙" "丁" "戊" "己" "庚" "辛" "壬" "癸"])
+  (calendar-chinese-terrestrial-branch ["子" "丑" "寅" "卯" "辰" "巳" "午" "未" "申" "酉" "戌" "亥"])
+  ;; 设置中文月份
+  (calendar-month-name-array ["一月" "二月" "三月" "四月" "五月" "六月" "七月" "八月" "九月" "十月" "十一月" "十二月"])
+  ;; 设置星期标题显示
+  (calendar-day-name-array ["日" "一" "二" "三" "四" "五" "六"])
+  ;; 周一作为一周第一天
+  (calendar-week-start-day 1)
+  )
+
+;; 中国节日设置
+(use-package cal-china-x
+  :ensure t
+  :commands cal-china-x-setup
+  :hook (after-init . cal-china-x-setup)
+  :config
+  ;; 重要节日设置
+  (setq cal-china-x-important-holidays cal-china-x-chinese-holidays)
+  ;; 所有节日设置
+  (setq cal-china-x-general-holidays
+        '(;;公历节日
+          (holiday-fixed 1 1 "元旦")
+          (holiday-fixed 2 14 "情人节")
+          (holiday-fixed 3 8 "妇女节")
+          (holiday-fixed 3 14 "白色情人节")
+          (holiday-fixed 4 1 "愚人节")
+          (holiday-fixed 5 1 "劳动节")
+          (holiday-fixed 5 4 "青年节")
+          (holiday-float 5 0 2 "母亲节")
+          (holiday-fixed 6 1 "儿童节")
+          (holiday-float 6 0 3 "父亲节")
+          (holiday-fixed 9 10 "教师节")
+          (holiday-fixed 10 1 "国庆节")
+          (holiday-fixed 10 2 "国庆节")
+          (holiday-fixed 10 3 "国庆节")
+          (holiday-fixed 10 24 "程序员节")
+          (holiday-fixed 11 11 "双11购物节")
+          (holiday-fixed 12 25 "圣诞节")
+          ;; 农历节日
+          (holiday-lunar 12 30 "春节" 0)
+          (holiday-lunar 1 1 "春节" 0)
+          (holiday-lunar 1 2 "春节" 0)
+          (holiday-lunar 1 15 "元宵节" 0)
+          (holiday-solar-term "清明" "清明节")
+          (holiday-solar-term "小寒" "小寒")
+          (holiday-solar-term "大寒" "大寒")
+          (holiday-solar-term "立春" "立春")
+          (holiday-solar-term "雨水" "雨水")
+          (holiday-solar-term "惊蛰" "惊蛰")
+          (holiday-solar-term "春分" "春分")
+          (holiday-solar-term "谷雨" "谷雨")
+          (holiday-solar-term "立夏" "立夏")
+          (holiday-solar-term "小满" "小满")
+          (holiday-solar-term "芒种" "芒种")
+          (holiday-solar-term "夏至" "夏至")
+          (holiday-solar-term "小暑" "小暑")
+          (holiday-solar-term "大暑" "大暑")
+          (holiday-solar-term "立秋" "立秋")
+          (holiday-solar-term "处暑" "处暑")
+          (holiday-solar-term "白露" "白露")
+          (holiday-solar-term "秋分" "秋分")
+          (holiday-solar-term "寒露" "寒露")
+          (holiday-solar-term "霜降" "霜降")
+          (holiday-solar-term "立冬" "立冬")
+          (holiday-solar-term "小雪" "小雪")
+          (holiday-solar-term "大雪" "大雪")
+          (holiday-solar-term "冬至" "冬至")
+          (holiday-lunar 5 5 "端午节" 0)
+          (holiday-lunar 8 15 "中秋节" 0)
+          (holiday-lunar 7 7 "七夕情人节" 0)
+          (holiday-lunar 12 8 "腊八节" 0)
+          (holiday-lunar 9 9 "重阳节" 0)))
+  ;; 设置日历的节日，通用节日已经包含了所有节日
+  (setq calendar-holidays (append cal-china-x-general-holidays)))
+
 ;;----------------------------------------------------------
 ;; org-capture
 ;; org-capture 的功能就是脑海中突然涌现了一个灵感，然后记录下来
